01e1b9700ba110a017e62c84df23253b2ab5f819
Stephanie Gatt <stephanielynngatt@gmail.com>
2020-06-12 11:44:24 -0400

    characters rendered to app

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characterData, setCharacterData] = useState([]);
+  
+  
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+      .then((response) => {
+        setCharacterData(response.data.results)
+        console.log("this is my data", response.data)
+      })
+      .catch((error) => {
+        console.log("No Bueno", error)
+      })
+  }, [])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+      characterData.map((data, i) => (
+       <Character character={data} key={i} /> 
+      ))
+      }
+
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React, { useState } from "react";
+// import Axios from "axios";
+
+const Character = (props) => {
+
+    return (
+        <div>
+         <h2>{props.character.name}</h2>
+        </div>
+      );
+}
+
+export default Character
